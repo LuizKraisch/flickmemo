11ffaf9ea60ce0fa2f911fe7753e0c13e45856eb
LuizKraisch <luizkraisch@trustedhealth.com>
2023-11-19 15:56:57 -0300

    chore: update status page url

--- lib/services/helpers.dart
+++ lib/services/helpers.dart
@@ -33,7 +33,7 @@ Future<void> launchDeveloperEmail() async {
 
 Future<void> launchStatusPage() async {
   final statusPageUrl =
-      Uri.parse('https://luizkraisch.github.io/flickmemo-status/');
+      Uri.parse('https://github.com/LuizKraisch/flickmemo-status#readme');
   if (await canLaunchUrl(statusPageUrl)) {
     await launchUrl(statusPageUrl);
   } else {
