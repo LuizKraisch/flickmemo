b861190db123726fb76f7a1daf06730d838e1397
LuizKraisch <luizkraisch@trustedhealth.com>
2023-11-23 10:51:58 -0300

    fix: env file

--- lib/env.dart
+++ lib/env.dart
@@ -1,6 +1,6 @@
 // Use this file to store all the sentitive data.
 // Remember to not commit this file with your data if you're forking the project.
 
-const String baseApiUrl = 'http://localhost:3000';
-const String tmdbImagesUrl = 'https://www.themoviedb.org/t/p/original';
-const String devEmail = 'luizkraisch22@gmail.com';
+const String baseApiUrl = 'your-api-url-here';
+const String tmdbImagesUrl = 'get-the-images-url-on-tmdb-website';
+const String devEmail = 'your-email-here';
